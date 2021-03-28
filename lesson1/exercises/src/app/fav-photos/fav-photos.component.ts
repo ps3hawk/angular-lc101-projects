3485dc41e8d771cfa71228a4092b17a71734a129
Matt Plog <ps3hawk@gmail.com>
2021-03-28 17:09:01 -0500

    photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Just a little something...';
+  image1 = 'https://github.com/ps3hawk/photos/blob/main/School/Charcoal/20200617_222948.jpg?raw=true';
+  image2 = 'assets/img/1.jpg';
+  image3 = 'assets/img/2.jpg';
 
   constructor() { }
 
